6435ddf9720567cd56497ce811bb674e5ed323a0
Yonatan Laurence <yonatanlaurence@gmail.com>
2025-07-24 18:55:55 +0300

    Fix question mark issue for Hebrew questions

--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -4,20 +4,28 @@ import { isRTL } from '../i18n/t.js';
 const Question = ({ level, question, questionIndex, answer, recordAnswer, scored }) => {
     const isCorrect = answer === question.correctAnswerIndex;
     const scoreMark = scored ? isCorrect ? '✅' : '❌' : '';
-    const FormattedQuestion = () => (
-        <div className="font-semibold mb-2">
-            {isRTL() ?
-            <>
-                <span>{question.question} </span>{'\u200e'}.<span>{questionIndex + 1}</span>
-                <span className="ml-2">{scoreMark}</span>
-            </> :
-            <>
-                <span className="mr-2">{scoreMark}</span>
-                <span>{questionIndex + 1}</span>. <span>{question.question}</span>
-            </>
-        }
-        </div>
-    );
+    const FormattedQuestion = () => {
+        // Disclaimer: supporting both LTR and RTL languages in one UI is tricky!!
+        // In Hebrew we get the question text with the question mark in the wrong place.
+        // So here we're removing it and adding it back in the correct place.
+        // Further down you'll see we're using the invisible LTR \u200e; character to force
+        // the number and period characters of the question to be displayed in the correct direction.
+        const questionTextWithoutQuestionMark = question.question.replace(/\?$/, '');
+        return (
+            <div className="font-semibold mb-2">
+                {isRTL() ?
+                <>
+                    <span>?</span><span>{questionTextWithoutQuestionMark}</span>{'\u200e'}.<span>{questionIndex + 1}</span>
+                    <span className="ml-2">{scoreMark}</span>
+                </> :
+                <>
+                    <span className="mr-2">{scoreMark}</span>
+                    <span>{questionIndex + 1}</span>. <span>{questionTextWithoutQuestionMark}</span><span>?</span>
+                </>
+            }
+            </div>
+        )
+    };
     return (
         <div className={`mb-6 ${isRTL() ? 'text-right' : ''}`}>
             <FormattedQuestion />
